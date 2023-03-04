ec48a673f10ca4c4c99c57044d19ed0ed96ff78a
Shogo Mizuno <shogo-mizuno@c-fo.com>
2023-03-04 06:12:34 +0000

    fix nvim-tree.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,9 +1,6 @@
 return {
   'nvim-tree/nvim-tree.lua',
   dependencies={'nvim-tree/nvim-web-devicons'},
-  keys = {
-    { "fe", ":NvimTreeToggle<cr>", desc = "NvimTreeToggle" },
-  },
   init = function()
     -- disable netrw at the very start of your init.lua (strongly advised)
     vim.g.loaded_netrw = 1
@@ -11,18 +8,28 @@ return {
     -- set termguicolors to enable highlight groups
     vim.opt.termguicolors = true
   end,
-  config = {
-    sort_by = "case_sensitive",
-    view = {
-      width = 40,
-      mappings = {
-        list = {
-          { key = "s", action = "" },
+
+  keys = {
+    { "<leader>ee", ":NvimTreeToggle<cr>", desc = "NvimTreeToggle" },
+    { "<leader>ef", ":NvimTreeFindFile<cr>", desc = "NvimTreeFindFile" },
+  },
+
+  config = function()
+    require("nvim-tree").setup( {
+      sort_by = "case_sensitive",
+      view = {
+        width = 40,
+        mappings = {
+          list = {
+            { key = "s", action = "" },
+          },
         },
       },
-    },
-    renderer = {
-      group_empty = true,
-    },
-  },
+      renderer = {
+        group_empty = true,
+      },
+    })
+  end,
 }
+
+
