4c854c42602503075481d637471e88c6704d982e
Shogo Mizuno <shogo-mizuno@c-fo.com>
2023-03-04 06:13:20 +0000

    fix null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,9 +6,11 @@ return {
 
     null_ls.setup({
       sources = {
+        null_ls.builtins.formatting.prettier.with{
+          prefer_local = "node_modules/.bin",
+        },
+--        null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
       },
     })
   end
