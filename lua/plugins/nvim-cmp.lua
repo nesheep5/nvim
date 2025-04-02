36e370cea7229389e4c84bd663295d6d190dc2f9
Shogo Mizuno <shogo-mizuno@c-fo.com>
2025-04-02 22:28:25 +0900

    cmp設定を更新

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,46 +8,106 @@ return {
     'hrsh7th/cmp-cmdline',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
+    {
+      'zbirenbaum/copilot.lua',
+      cmd = "Copilot",
+      event = "InsertEnter",
+    },
+    {
+      'zbirenbaum/copilot-cmp',
+      dependencies = { 'zbirenbaum/copilot.lua' },
+    },
+    {
+      'petertriho/cmp-git',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        require("cmp_git").setup()
+      end
+    },
   },
+
   config = function()
     local cmp = require 'cmp'
 
+    -- Copilot setup
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+    require("copilot_cmp").setup()
+
     cmp.setup({
       snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
           require('luasnip').lsp_expand(args.body)
         end,
       },
       window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        format = function(entry, vim_item)
+          local source_names = {
+            nvim_lsp = "[LSP]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+            luasnip = "[Snippet]",
+            copilot = "[Copilot]",
+            cmp_git = "[Git]",
+          }
+          vim_item.menu = source_names[entry.source.name] or entry.source.name
+          return vim_item
+        end,
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif require("luasnip").expand_or_jumpable() then
+            require("luasnip").expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            require("luasnip").jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
+
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        { name = 'copilot',  group_index = 1 },
+        { name = 'nvim_lsp', group_index = 2 },
+        { name = 'luasnip',  group_index = 2 },
+        { name = 'path',     group_index = 2 },
       }, {
-        { name = 'buffer', option = { keyword_pattern = [[\k\+]] } },
+        { name = 'buffer', group_index = 3 },
       })
     })
 
-    -- Set configuration for specific filetype.
+    -- Git commit message 用補完
     cmp.setup.filetype('gitcommit', {
       sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+        { name = 'cmp_git' },
       }, {
         { name = 'buffer' },
       })
     })
 
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    -- 検索（/ or ?）用
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
@@ -55,7 +115,7 @@ return {
       }
     })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    -- コマンドライン補完（:）
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
