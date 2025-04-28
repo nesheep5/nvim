1f9bab428faef5e8553a79e22493d69f260613e5
Shogo Mizuno <shogo-mizuno@c-fo.com>
2025-04-28 12:05:42 +0900

    コピー系キーマップを追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,3 @@
-vim.keymap.set("i", "jj", "<ESC>")
-
 -- window
 vim.keymap.set("n", "ss", "<c-w>s")
 vim.keymap.set("n", "sv", "<c-w>v")
@@ -14,3 +12,24 @@ vim.keymap.set("n", "<leader>,", ":e ~/.config/nvim/init.lua<cr>")
 vim.keymap.set("n", "<leader>gy", function()
   require("utils.github_url").copy_github_url()
 end, { desc = "Copy GitHub URL of current line" })
+
+-- 相対パスをコピーするキーマッピング
+vim.keymap.set('n', '<leader>cp', function()
+  local relative_path = vim.fn.fnamemodify(vim.fn.expand('%'), ':.')
+  vim.fn.setreg('+', relative_path)
+  vim.notify('Copied to clipboard: ' .. relative_path)
+end, { desc = 'Copy relative file path' })
+
+-- 設定リロード用のキーマッピング
+vim.keymap.set('n', '<leader>r', function()
+  -- Luaのキャッシュをクリア
+  for name, _ in pairs(package.loaded) do
+    if name:match('^user') or name:match('^plugins') then
+      package.loaded[name] = nil
+    end
+  end
+
+  -- 設定を再読み込み
+  dofile(vim.env.MYVIMRC)
+  vim.notify('Config reloaded!', vim.log.levels.INFO)
+end, { desc = 'Reload config' })
