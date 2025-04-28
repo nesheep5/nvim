55154ca1376580bd81eb9dd72a506d2333afe2b5
Shogo Mizuno <shogo-mizuno@c-fo.com>
2025-04-28 12:09:45 +0900

    miseのshimsをPATHに追加

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- miseはPATHを動的に作成するので、shimsを設定する
+--vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
 vim.g.mapleader = " " -- vim.keymapより前に置くこと
 
 require("config.options")
